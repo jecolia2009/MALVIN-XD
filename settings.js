e0ff69b55aaf0b37de133f3b55bf2ce094a658f3
jecolia2009 <jecoliamarilyseouihila2@gmail.com>
2025-07-17 13:03:04 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID (keep secure)
+  SESSION_ID: process.env.SESSION_ID || "malvin~Qmk3GJbT#4RRQGf17bE7LP1DPHUH4m9fY2a0EClG3upWBHsrftcc", // Your bot's session ID (keep secure)
   PREFIX: getConfig("PREFIX") || ".", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴍᴀʟᴠɪɴ-xᴅ", // Bot display name
